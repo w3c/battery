99ef943cfda78c6a0a0de5091a4a956db48fd051
Anssi Kostiainen <anssi.kostiainen@nokia.com>
2012-11-22 15:26:01 +0200

    split the prime computation into smaller chunck to keep the UI thread responsive

--- tests/submissions/anssik/prime.js
+++ tests/submissions/anssik/prime.js
@@ -1,4 +1,4 @@
-// http://html5demos.com/worker
+// adapted from http://html5demos.com/worker
 
 var running = false;
 
@@ -6,20 +6,30 @@ onmessage = function (event) {
   // doesn't matter what the message is, just toggle the worker
   if (running == false) {
     running = true;
-    run();
+    run(1);
   } else {
     running = false;
   }
 };
 
-function run() {
-  var n = 1;
-  search: while (running) {
+function run(n) {
+  // split the task into 20k chunks
+  var limit = n + 20000;
+  search: while (running && n < limit) {
     n += 1;
-    for (var i = 2; i <= Math.sqrt(n); i += 1)
-      if (n % i == 0)
-       continue search;
+    for (var i = 2; i <= Math.sqrt(n); i += 1) {
+      if (n % i == 0) {
+        continue search;
+      }
+    }
     // found a prime!
     postMessage(n);
+  }
+  if (n === limit) {
+    // wait for the UI thread to update itself
+    setTimeout(function(start_time) {
+      // resume prime computation at n
+      run(n);
+    }, 150);
   }
 }
